675ef77bfb248fcf1284d5a4254ecbdc2bddf873
rubenaprikyan <rubenaprikyan@gmail.com>
2024-09-24 21:12:10 +0400

    feat: Add database connection errors normalizer

--- backend/src/config/enums.ts
+++ backend/src/config/enums.ts
@@ -2,8 +2,3 @@ export enum Order {
   ASC = 'asc',
   DESC = 'desc',
 }
-
-export enum PrismaExceptionCodes {
-  UniqueConstraint = 'P2002',
-  RecordNotFound = 'P2025',
-}
--- backend/src/middlewares/errorHandler.ts
+++ backend/src/middlewares/errorHandler.ts
@@ -1,10 +1,40 @@
 import express from 'express';
-import { BaseError, InternalServerError } from '../modules/exceptions';
+import {
+  BaseError,
+  InternalServerError,
+  UnavailableServiceError,
+} from '@/modules/exceptions';
+
+function normalizePrismaErrors(error: any) {
+  // TODO move to readable enums
+  const databaseConnectionErrors = [
+    'P1000',
+    'P1001',
+    'P1002',
+    'P1003',
+    'P1008',
+    'P1009',
+    'P1010',
+    'P1011',
+  ];
+
+  if (error?.name === 'PrismaClientKnownRequestError') {
+    if (databaseConnectionErrors.includes(error.code)) {
+      return new UnavailableServiceError('Database Service is Unavailable.');
+    }
+  }
+}
 
 function normalizeError(err: Error): BaseError {
   // if its already normalized error, just return
   if (err instanceof BaseError) {
     return err;
+  }
+
+  // normalize database errors
+  const prismaError = normalizePrismaErrors(err);
+  if (prismaError) {
+    return prismaError;
   }
 
   // Here is the place to Normalize exceptions from
